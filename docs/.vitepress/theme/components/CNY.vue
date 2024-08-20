bbff638c829c1ecb848a8d3b699b9a1a11e443cc
plus <ljw@live.jp>
2024-08-20 12:31:15 +0800

    Update CNY.vue

--- docs/.vitepress/theme/components/CNY.vue
+++ docs/.vitepress/theme/components/CNY.vue
@@ -1,8 +1,7 @@
 <script setup>
-import { definedSlots } from 'vue';
 import useCurrency from '../hooks/useCurrency';
 
-const slots = definedSlots();
+const slots = defineSlots();
 const { formattedAmount } = useCurrency(slots?.default?.()?.[0]?.children || 0);
 </script>
 
