f0f8c9237b02ea36e342c0643b5f408ab35b47de
plus <ljw@live.jp>
2024-08-20 12:13:20 +0800

    Update components

---
+++ docs/.vitepress/theme/components/Currency.vue
@@ -0,0 +1,11 @@
+<script setup>
+import useCurrency from '../hooks/useCurrency';
+
+const { currency, currencies } = useCurrency();
+</script>
+
+<template>
+  <select>
+    <option v-for="(c, name) in currencies" :value="name">{{ name }}({{ c.unit }})</option>
+  </select>
+</template>
