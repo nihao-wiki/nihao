ec257a1d0383dcbd31cb473d970dd36d4b636bd6
plus <ljw@live.jp>
2024-09-03 17:20:45 +0800

    feat: clarity

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -45,6 +45,15 @@ export default defineConfig({
       gtag('js', new Date());
       gtag('config', 'G-7L0HBZJ593');`
     ],
+    [
+      'script',
+      {},
+      `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "nnl0485bb7");`
+    ],
   ],
   markdown: {
     image: {
